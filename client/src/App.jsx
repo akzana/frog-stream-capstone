4a6140a5251e064b68497df931f2bcdc8f45fe2a
akzana <akzanaklepsch@outlook.com>
2024-11-26 10:32:02 -0800

    🚧 start app routing

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,16 @@
-import './App.scss'
+import {BrowserRouter, Routes, Route} from "react-router-dom";
+import './App.scss';
 
 function App() {
 
   return (
+    <BrowserRouter>
     "hello world"
+    <Routes>
+      <Route />
+    </Routes>
+    </BrowserRouter>
+    
   )
 }
 
