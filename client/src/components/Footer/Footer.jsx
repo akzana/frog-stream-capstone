7e0094a9ee657ad05f0a4586845c29428fae4479
akzana <akzanaklepsch@outlook.com>
2024-11-28 09:56:32 -0800

    🚧 add content to Footer

--- client/src/components/Footer/Footer.jsx
+++ client/src/components/Footer/Footer.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import "./Footer.scss";
+import { Link } from 'react-router-dom';
 
 export default function Footer() {
   return (
     <div>
-      FOOTER
+      <Link to="/">Home</Link>
+      <Link to="LogInPage">Become a Creator</Link>
     </div>
   )
 }
