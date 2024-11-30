faac2d56432d1771181f172ce24927ce257b2a7b
akzana <akzanaklepsch@outlook.com>
2024-11-29 19:10:46 -0800

    🔨 add VideoPlayer

--- client/src/pages/CreatorStreamPage/CreatorStreamPage.jsx
+++ client/src/pages/CreatorStreamPage/CreatorStreamPage.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import "./CreatorStreamPage.scss";
+import VideoPlayer from '../../components/VideoPlayer/VideoPlayer.jsx';
+
 
 export default function CreatorStreamPage() {
   return (
     <div>
-      CreatorStreamPage
+      <VideoPlayer src="https://www.w3schools.com/html/mov_bbb.mp4" />
     </div>
   )
 }
